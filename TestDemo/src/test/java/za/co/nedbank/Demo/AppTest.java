37ab029f975981e69a200cbc9fe9d3f4cd567184
ramprasad336 <ramprasad.prathapani@gmail.com>
2019-06-05 22:54:59 +0200

    First commit

---
+++ TestDemo/src/test/java/za/co/nedbank/Demo/AppTest.java
@@ -0,0 +1,16 @@
+package za.co.nedbank.Demo;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    @Test
+    public void exampleOfTestNgMaven()
+    {
+        System.out.println(" START OF EXECUTION :testng.xmlThis is TestNG-Maven Example");
+    }
+
+}
